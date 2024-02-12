7919cd6aa32aec554efee74830e32abce3534d80
Thomas NACHAR <thomas.nachar18@gmail.com>
2024-02-12 20:16:38 +0100

    index

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/styles/style.css">
+</head>
+<body>
+    <div class="container">
+        <section>
+            <div class="book">
+                <div class="cover">
+                    <h2>Sabrina Pinthon</h2>
+                    <img src="./assets/img/logononame.svg" alt="">
+                    <h1>Berry Very Gourmande</h1>
+                </div>
+            </div>
+        </section>
+        <div class="links">
+            <div id="facebook">
+                <img src="./assets/img/logofacebook.svg" alt="">
+                <img src="./assets/img/logoinstagram.svg" alt="">
+                <img src="./assets/img/logoX.svg" alt="">
+                <img src="./assets/img/logoIn.svg" alt="">
+            </div>
+        </div>
+        <button class="discover">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="48px" height="48px">
+                <path d="M0 0h24v24H0z" fill="none"/>
+                <path d="M8 5v14l11-7z"/>
+            </svg>
+        </button>
+    </div>
+</body>
+</html>
