65128aa56f898197dafd36a3e080d7a3dbac59a4
Thomas NACHAR <thomas.nachar18@gmail.com>
2024-02-12 21:18:57 +0100

    frontDone

--- index.php
+++ index.php
@@ -8,22 +8,28 @@
 </head>
 <body>
     <div class="container">
+        <p>Maintenir enfoncé pour retourner</p>
         <section>
             <div class="book">
                 <div class="cover">
                     <h2>Sabrina Pinthon</h2>
-                    <img src="./assets/img/logononame.svg" alt="">
+                    <img src="./assets/img/logononame.svg" alt="Logo BVG">
                     <h1>Berry Very Gourmande</h1>
+                </div>
+                <div class="backcover">
+                    <img src="./assets/img/creatrice-bvg.jpg" alt="Sabrina Pinthon image">
+                    <h2>Sabrina Pinthon</h2>
+                    <p>Créatrice de Berry Very Gourmande, 43 ans, libraire depuis 17 ans.</p>
+                    <p>J’aime la littérature, la gastronomie, et découvrir les différentes spécialités culinaires des différentes régions.</p>
+                    <p>Vous pouvez me retrouver sur mes réseaux sociaux ainsi que me contacter via mon mail ou numéro de téléphone.</p>
                 </div>
             </div>
         </section>
         <div class="links">
-            <div id="facebook">
-                <img src="./assets/img/logofacebook.svg" alt="">
-                <img src="./assets/img/logoinstagram.svg" alt="">
-                <img src="./assets/img/logoX.svg" alt="">
-                <img src="./assets/img/logoIn.svg" alt="">
-            </div>
+                <img src="./assets/img/logofacebook.svg" alt="Facebook">
+                <img src="./assets/img/logoinstagram.svg" alt="Instagram">
+                <img src="./assets/img/logoX.svg" alt="X/Twitter">
+                <img src="./assets/img/logoIn.svg" alt="LinkedIn">
         </div>
         <button class="discover">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="48px" height="48px">
@@ -31,6 +37,7 @@
                 <path d="M8 5v14l11-7z"/>
             </svg>
         </button>
+        <p>Appuyer sur le bouton pour commencer</p>
     </div>
 </body>
 </html>
